966c2e45f0bdb323fabd076ca8cc80566c8906ac
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-25 23:24:39 +0900

    chatting app ui

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chatting/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chatting/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginSignupScreen(),
+    );
+  }
+}
