12d50d4b193207ffeffb2c75c6aefcdb41292893
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-30 18:24:26 +0900

    채팅 구현을 위한 Stream 적용, showSpinner

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -49,8 +50,29 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Chat screen'),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/nJVc6hsknkgtMIAi7Vay/message')
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.all(8),
+                child: Text(docs[index]['text']),
+              );
+            },
+          );
+        },
       ),
     );
   }
