881a432f2a848137ffee4c93b65a7db1899f4af2
kwonjs1996 <kwonjs1996@gmail.com>
2022-05-31 20:28:11 +0900

    Chatscreen 메세지 보내기

--- lib/chatting/chat/new_messages.dart
+++ lib/chatting/chat/new_messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NewMessage extends StatefulWidget {
   const NewMessage({Key? key}) : super(key: key);
@@ -9,6 +10,12 @@ class NewMessage extends StatefulWidget {
 
 class _NewMessageState extends State<NewMessage> {
   var _userEnterMessage = '';
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': _userEnterMessage,
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +35,7 @@ class _NewMessageState extends State<NewMessage> {
             ),
           ),
           IconButton(
-            onPressed: _userEnterMessage.trim().isEmpty ? null : () {},
+            onPressed: _userEnterMessage.trim().isEmpty ? null : _sendMessage,
             icon: Icon(Icons.send),
             color: Colors.blue,
           )
